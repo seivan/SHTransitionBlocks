96ce062f05448b1e23a7cd0d3dd2e09cb2cad783
Seivan Heidari <seivan@kth.se>
2013-08-05 12:57:45 +0200

    Add new target files.

---
+++ Example/ExampleTests/ExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  ExampleTests.h
+//  ExampleTests
+//
+//  Created by Seivan Heidari on 8/5/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ExampleTests : SenTestCase
+
+@end
