9877325fbef815aa5bf81f747f4680b300036c74
Seivan Heidari <seivan@kth.se>
2013-08-05 09:31:32 +0200

    Add tests

---
+++ Example/ExampleTests/ExampleTests.m
@@ -0,0 +1,32 @@
+//
+//  ExampleTests.m
+//  ExampleTests
+//
+//  Created by Seivan Heidari on 8/5/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+#import "ExampleTests.h"
+
+@implementation ExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ExampleTests");
+}
+
+@end
